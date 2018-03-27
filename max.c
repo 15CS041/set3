65532d3956445df116cb69e4f335d05f0f74cd56
15CS041 <rajaudhayaap1998@gmail.com>
2018-03-27 15:44:04 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,13 @@
+int maxValue( int myArray [], int size)
+{
+    int i, maxValue;
+    maxValue=myArray[0];
+
+    //find the largest no
+    for (i=0;i)
+        {
+        if (myArray[i]>maxValue)
+        maxValue=myArray[i];
+        }   
+        return maxValue;
+}
